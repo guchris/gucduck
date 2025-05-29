784deebcebfea554d02fcd17acf8bf88ab99bc46
Christopher Gu <cleegu@gmail.com>
2025-05-29 02:04:35 -0700

    sunday-supper rename to dish-dish

--- app/food/dish-dish/page.tsx
+++ app/food/dish-dish/page.tsx
@@ -20,7 +20,7 @@ const suppers = [
   },
 ];
 
-export default function SundaySuppersPage() {
+export default function DishDishPage() {
   return (
     <div className="min-h-screen flex flex-col gap-4 p-4">
       <header className="w-full">
@@ -29,14 +29,14 @@ export default function SundaySuppersPage() {
       <main className="flex-1 flex flex-col items-center gap-4">
         <Card className="w-full rounded-none shadow-none border-dashed border-gray-300">
           <CardHeader className="items-start p-4">
-            <CardTitle className="text-lg font-bold text-left">Sunday Suppers</CardTitle>
-            <CardDescription className="text-sm text-left mt-2">
-              Sunday Suppers is my cherished weekly tradition with my best friend Anjuli. It's our way of blending the joy of cooking with genuine connection every weekend. Throughout the week, we find and choose a recipe, split the ingredients, and cook together at my place. It's more than just a meal; it's an opportunity for deep conversations and laughter as we savor the process.<br /><br />
-              We capture our culinary creations in photos, indulge in the delicious results, and then add a unique touch — scoring and ranking each dish against previous ones. This ritual combines my love for cooking and quality time, making memories and forming deeper connections.
+            <CardTitle className="text-lg md:text-3xl font-bold text-left">Dish Dish</CardTitle>
+            <CardDescription className="text-left mt-2">
+              Dish Dish is me and my best friend Anjuli’s favorite tradition. Every so often, we pick a recipe from social media, split the grocery list, and meet up at my place to cook, catch up, and laugh about everything and nothing.<br /><br />
+              It’s not just about the food (though that part’s great) — it’s our way of making time for each other. We always snap pics of our creations, eat way too much, and then rate the dish like we’re judges on a cooking show.
             </CardDescription>
           </CardHeader>
         </Card>
-        {/* <Card className="w-full rounded-lg shadow-none">
+        <Card className="w-full rounded-none shadow-none border-dashed border-gray-300">
           <CardHeader className="items-start p-4">
             <CardTitle className="text-lg font-bold text-left">Scoring System</CardTitle>
             <CardDescription className="text-sm text-left">
@@ -45,7 +45,7 @@ export default function SundaySuppersPage() {
           </CardHeader>
           <CardContent className="px-4 pt-0">
             <div className="grid gap-4 md:grid-cols-2">
-              <Card className="rounded-lg shadow-none">
+              <Card className="rounded-none shadow-none border-dashed border-gray-300">
                 <CardHeader className="flex-row items-center gap-2 p-4 pb-0">
                   <CardTitle className="flex items-center gap-2 text-lg font-semibold">
                     Taste <Badge variant="secondary">10 pts</Badge>
@@ -53,12 +53,11 @@ export default function SundaySuppersPage() {
                 </CardHeader>
                 <CardContent className="pt-2 pb-4 px-4">
                   <CardDescription>
-                    The overall flavor profile and how enjoyable the dish is.<br />
-                    Does the food taste good? Does the dish come together?
+                    The overall flavor profile and how enjoyable the dish is. Does the food taste good? Does the dish come together?
                   </CardDescription>
                 </CardContent>
               </Card>
-              <Card className="rounded-lg shadow-none">
+              <Card className="rounded-none shadow-none border-dashed border-gray-300">
                 <CardHeader className="flex-row items-center gap-2 p-4 pb-0">
                   <CardTitle className="flex items-center gap-2 text-lg font-semibold">
                     Appearance <Badge variant="secondary">5 pts</Badge>
@@ -66,12 +65,11 @@ export default function SundaySuppersPage() {
                 </CardHeader>
                 <CardContent className="pt-2 pb-4 px-4">
                   <CardDescription>
-                    The visual appeal and arrangement of the dish.<br />
-                    Is the food Instagram-worthy? Does the dish come together?
+                    The visual appeal and arrangement of the dish. Is the food Instagram-worthy? Does the dish come together?
                   </CardDescription>
                 </CardContent>
               </Card>
-              <Card className="rounded-lg shadow-none">
+              <Card className="rounded-none shadow-none border-dashed border-gray-300">
                 <CardHeader className="flex-row items-center gap-2 p-4 pb-0">
                   <CardTitle className="flex items-center gap-2 text-lg font-semibold">
                     Effort <Badge variant="secondary">5 pts</Badge>
@@ -79,12 +77,11 @@ export default function SundaySuppersPage() {
                 </CardHeader>
                 <CardContent className="pt-2 pb-4 px-4">
                   <CardDescription>
-                    The dedication and skill required in preparing the dish.<br />
-                    How long did the dish demand? How labor-intensive or intricate was the process?
+                    The dedication and skill required in preparing the dish. How long did the dish demand? How labor-intensive or intricate was the process?
                   </CardDescription>
                 </CardContent>
               </Card>
-              <Card className="rounded-lg shadow-none">
+              <Card className="rounded-none shadow-none border-dashed border-gray-300">
                 <CardHeader className="flex-row items-center gap-2 p-4 pb-0">
                   <CardTitle className="flex items-center gap-2 text-lg font-semibold">
                     Misc <Badge variant="secondary">5 pts</Badge>
@@ -92,14 +89,13 @@ export default function SundaySuppersPage() {
                 </CardHeader>
                 <CardContent className="pt-2 pb-4 px-4">
                   <CardDescription>
-                    The memorability and creativity of the dish.<br />
-                    Is the food made in a creative way? Is it a unique cooking experience?
+                    The memorability and creativity of the dish. Is the food made in a creative way? Is it a unique cooking experience?
                   </CardDescription>
                 </CardContent>
               </Card>
             </div>
           </CardContent>
-        </Card> */}
+        </Card>
         <Card className="w-full rounded-none shadow-none border-dashed border-gray-300">
           <CardContent className="p-0">
             {suppers.map((supper, i) => (
